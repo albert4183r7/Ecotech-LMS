64b6e9dc46eab18f6b5f87ce5d8c52d727e3e3bf
albert4183r7 <albertlie8338@gmail.com>
2026-08-19 22:40:46 +0700

    Remove agent scratch output, QA screenshots and template leftovers

--- start-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  NODE_OPTIONS='--dns-result-order=ipv4first' npx next dev -p 3000 2>&1 | tee -a dev.log
-  echo "[$(date)] Server died, restarting in 2s..." >> dev.log
-  sleep 2
-done
